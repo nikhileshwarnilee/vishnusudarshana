973aece69e9eb4684b67eb5c06f2a07e53cb613c
nikhileshwarnilee <nikhileshwarnilee@gmail.com>
2026-01-19 14:06:58 +0530

    c

--- admin/index.php
+++ admin/index.php
@@ -100,7 +100,6 @@ $recentRows = $recentStmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Marcellus&family=UnifrakturCook:wght@700&display=swap">
     <link rel="stylesheet" href="/assets/css/style.css">
         <style>
         body {
